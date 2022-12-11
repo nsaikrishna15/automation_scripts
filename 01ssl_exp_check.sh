1bc9b46d34b430a4bd9d43ea46b160ae7428aa08
saikrishna narvaneni <nsaikrishna1515@gmail.com>
2022-12-12 00:32:10 +0530

    q&a updated

--- 01ssl_exp_check.sh
+++ 01ssl_exp_check.sh
@@ -8,8 +8,9 @@ for website in "${websites[@]}"
 do
     # Check the SSL expiration date using the openssl command
     expiry=$(echo | openssl s_client -servername "$website" -connect "$website":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -d'=' -f2)
-    certificate=$(echo | openssl s_client -servername "$website" -connect "$website":443 2>/dev/null | openssl x509)
+    # certificate=$(echo | openssl s_client -servername "$website" -connect "$website":443 2>/dev/null | openssl x509)
 
     # Print the website and its SSL expiration date
     echo "$website: $expiry: $certificate" 
-done | sort -k2 > log.csv
+done | sort -k2 > log.txt
+
--- 03array.py
+++ 03array.py
@@ -1,24 +1,40 @@
 import random
 
-q = [
-    "What is India's capital city? a. Hyderabad b. New Delhi c. Bangalore d. Mumbai",
-    "Who is the prime minister of India? a. Narendra Modi b. Pranab Mukharji c. Venkaiah Naidu d. Amit Shah",
-    "Who is the present Indian Cricket Team Captain? a. KL Rahul b. Virat Kohli c. Rohit Sharma d. Hardik Pandya"
+class Question:
+ def __init__(self, questionText, multipleChoiceOptions):
+    self.questionText = questionText
+    self.multipleChoiceOptions = multipleChoiceOptions
+
+quizQuestions = [
+  Question("What is India's capital city?", ["(a) Hyderabad", "(b) New Delhi", "(c) Bangalore", "(d) Mumbai"]),
+  Question("Who is the prime minister of India?", ["(a) Narendra Modi", "(b) Pranab Mukharji", "(c) Venkaiah Naidu", "(d) Amit Shah"]),
+  Question("Who is the present Indian Cricket Team Captain?", ["(a) KL Rahul", "(b) Virat Kohli", "(c) Rohit Sharma", "(d) Hardik Pandya"])
 ]
 
-a = []
+answersSubmitted = []
 
-for value in random.sample(q,len(q)):
-    print(f"{value}", end="\n")
-    answer = input("\nChoice? ")
+def ask_question(question):
+    print(f"{question.questionText}?")
+    for option in question.multipleChoiceOptions:
+        print(option)
+    answer = input("\nChoice? ") 
+    print("\n")
     if answer.lower() not in ('a', 'b', 'c', 'd', 'quit'):
         print("Accepted answers: 'a','b','c','d' or 'quit' to quit",end="\n\n")
-        continue
+        ask_question(question)
+    else:
+        if answer.lower() != "quit":
+            answersSubmitted.append((question.questionText,answer))
     if answer == "quit":
-        break
-    a.append((value,answer))
-
-print("Answers Submitted: ", end="\n\n")
+        print_question(answersSubmitted)
+        quit()
+        
+def print_question(answersSubmitted):
+    print("\nAnswers Submitted: ", end="\n\n")
+    for (question, answer) in answersSubmitted:
+        print(question + " : " + answer, end="\n\n")
     
-for (question, answer) in a:
-    print(question + " : " + answer, end="\n\n")
+for question in random.sample(quizQuestions,len(quizQuestions)):
+    ask_question(question)
+
+print_question(answersSubmitted)
